c20a9a06d220a63a2bc0f487ea6c802791142622
Rafael de Alkmim Dias <rafas07@hotmail.com>
2020-07-15 20:34:36 -0300

    fim

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  const handleButtonClick = () =>{
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello World" />
-      <div>{counter}</div>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+
+    <Routes />
+
   );
 }
 
